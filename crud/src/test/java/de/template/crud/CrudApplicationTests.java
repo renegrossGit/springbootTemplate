0424ce4e1d26fb12551154238c6468c932c76c87
renegrossGit <rene.gross@uni-konstanz.de>
2025-01-12 21:35:34 +0100

    crud template first step

---
+++ crud/src/test/java/de/template/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package de.template.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
